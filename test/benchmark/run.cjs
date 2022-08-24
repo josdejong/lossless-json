d39b3065b0a987c76ac10b84c766474e3ec7c60c
Jos de Jong <wjosdejong@gmail.com>
2022-08-24 09:16:30 +0200

    Update dependencies, improve  benchmark setup

---
+++ test/benchmark/run.cjs
@@ -0,0 +1,21 @@
+'use strict';
+
+const { readFileSync } = require('fs');
+const LosslessJSON = require('../../dist/lossless-json.js');
+const Benchmark = require('benchmark')
+
+const json = JSON.parse(readFileSync(__dirname + '/largefile.json', 'utf-8'));
+const text = JSON.stringify(json);
+
+const suite = new Benchmark.Suite()
+suite
+  .add('JSON.parse            ', () => JSON.parse(text))
+  .add('LosslessJSON.parse    ', () => LosslessJSON.parse(text))
+  .add('JSON.stringify        ', () => JSON.stringify(json))
+  .add('LosslessJSON.stringify', () => LosslessJSON.stringify(json))
+  .on('cycle', function (event) {
+    console.log(String(event.target))
+  })
+  .on('complete', function () {
+  })
+  .run()
