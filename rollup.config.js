8f8b568707e2d4eb217e40bb68ac22690faf913b
Jos de Jong <wjosdejong@gmail.com>
2022-08-26 16:46:36 +0200

    Minify the UMD bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
+import { terser } from 'rollup-plugin-terser'
+
 export default {
-  input: 'src/index.ts',
+  input: './lib/esm/index.js',
   output: {
     name: 'LosslessJSON',
     file: 'lib/umd/lossless-json.js',
     format: 'umd',
     compact: true,
-    sourcemap: true
+    sourcemap: true,
+    plugins: [terser()]
   }
 }
