77ee1887c5f5c73504d1bee5d60ca697963818ce
Jos de Jong <wjosdejong@gmail.com>
2022-08-29 09:36:41 +0200

    Test whether all util functions are exported

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,32 @@
-import { parse, stringify, LosslessNumber } from '../src'
+import {
+  getUnsafeNumberReason,
+  isInteger,
+  isLosslessNumber,
+  isNumber,
+  isSafeNumber,
+  LosslessNumber,
+  parse,
+  parseLosslessNumber,
+  parseNumberAndBigInt,
+  reviveDate,
+  stringify,
+  toLosslessNumber,
+  toSafeNumberOrThrow
+} from '../src'
 
 test('Public API', function () {
   expect(parse('{}')).toEqual({})
   expect(stringify({})).toEqual('{}')
   expect(new LosslessNumber('2').isLosslessNumber).toBe(true)
+
+  expect(typeof isLosslessNumber).toBe('function')
+  expect(typeof toLosslessNumber).toBe('function')
+  expect(typeof reviveDate).toBe('function')
+  expect(typeof parseLosslessNumber).toBe('function')
+  expect(typeof parseNumberAndBigInt).toBe('function')
+  expect(typeof isInteger).toBe('function')
+  expect(typeof isNumber).toBe('function')
+  expect(typeof isSafeNumber).toBe('function')
+  expect(typeof toSafeNumberOrThrow).toBe('function')
+  expect(typeof getUnsafeNumberReason).toBe('function')
 })
